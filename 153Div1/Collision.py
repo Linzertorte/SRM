8da5338310253de37a42951b3e4c1bb409a1bad7
Zhaoliang Liu <Linzertorte@users.noreply.github.com>
2014-07-09 19:09:43 -0400

    Create Collision.py

---
+++ 153Div1/Collision.py
@@ -0,0 +1,21 @@
+__author__ = 'qingfei'
+class Collision:
+    def probability(self, assignments, ids):
+        p1 = 1.0
+
+        m1 = ids
+        m2 = ids
+        if sum(assignments)>ids:
+            return 0.0
+        for assignment in assignments:
+            m2 = ids
+            for i in xrange(assignment):
+                p1*=(1.0*m1/m2)
+                m1-=1
+                m2-=1
+        p2 = 1.0
+        m = ids
+        for i in xrange(sum(assignments)):
+            p2 *= (1.0*m/ids)
+            m -= 1
+        return p1-p2
