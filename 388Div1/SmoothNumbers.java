431316d2a0c68c8d773c667f9381a328b8559232
Zhaoliang Liu <Linzertorte@users.noreply.github.com>
2014-07-23 07:49:16 -0400

    Create SmoothNumbers.java

---
+++ 388Div1/SmoothNumbers.java
@@ -0,0 +1,22 @@
+
+public class SmoothNumbers
+{
+    private int largestPrime(int n){
+        int largest = 1;
+        int i=2;
+        while(i*i<=n){
+            if(n%i==0) largest=Math.max(i,largest);
+            while(n%i==0) n/=i;
+            i++;
+        }
+        return Math.max(n,largest);
+    }
+    public int countSmoothNumbers(int N, int k)
+    {
+        int cnt = 0;
+        for(int i=1;i<=N;i++){
+            //System.out.println(largestPrime(i));
+            if(largestPrime(i)>k) cnt++;
+        }
+        return N-cnt;
+    }
